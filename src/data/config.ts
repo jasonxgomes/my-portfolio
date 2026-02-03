2ebfeba17efbf4be12de415a86110bc0f03db1f7
JasonxGomes <jasonxgomes@icloud.com>
2026-02-02 20:03:29 -0500

    Update hero section with my name and info

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,12 +1,12 @@
 const config = {
-  title: "Naresh Khatri | Full-Stack Developer",
+  title: "Jason Gomes | Full-Stack Developer",
   description: {
     long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
       "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
   },
   keywords: [
-    "Naresh",
+    "Jason",
     "portfolio",
     "full-stack developer",
     "creative technologist",
@@ -23,23 +23,23 @@ const config = {
     "Spline",
     "Framer Motion",
   ],
-  author: "Naresh Khatri",
-  email: "naresh.khatri2345@gmail.com",
+  author: "Jason Gomes",
+  email: "business.jasongomes@gmail.com",
   site: "https://nareshkhatri.site",
 
   // for github stars button
-  githubUsername: "naresh-khatri",
-  githubRepo: "3d-portfolio",
+  githubUsername: "jasonxgomes",
+  githubRepo: "my-portfolio",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
   },
   social: {
-    twitter: "https://x.com/nothotchaddi",
-    linkedin: "https://www.linkedin.com/in/naresh-khatri/",
-    instagram: "https://www.instagram.com/hotchaddi",
-    facebook: "https://www.facebook.com/HotChaddi/",
-    github: "https://github.com/Naresh-Khatri",
+    twitter: "https://x.com/",
+    linkedin: "https://",
+    instagram: "https://",
+    facebook: "https://",
+    github: "https://github.com/jasonxgomes",
   },
 };
 export { config };
