0d819132d5169651588103c541b303e3ec356d95
Jason Gomes <jasonxgomes@icloud.com>
2026-02-04 18:21:44 -0500

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -25,7 +25,7 @@ const config = {
   ],
   author: "Jason Gomes",
   email: "business.jasongomes@gmail.com",
-  site: "https://nareshkhatri.site",
+  site: "my-portfolio-eight-plum-82.vercel.app",
 
   // for github stars button
   githubUsername: "jasonxgomes",
